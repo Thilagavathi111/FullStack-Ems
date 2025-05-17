383a075da98d37b1213fe691c21e743e4500129e
Thilagavathi111 <thilagavathii.pushparaj@gmail.com>
2025-05-17 09:29:51 +0530

    Delete EmployeeManagementSystem-Backend1 directory

--- EmployeeManagementSystem-Backend1/src/main/java/com/employee/management/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.management.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
